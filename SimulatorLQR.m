ff43c65c631c4f10783f8fb77eb76926342f872a
FNR13 <152200095+FNR13@users.noreply.github.com>
2025-03-05 09:47:19 +0000

    added input saturation

--- SimulatorLQR.m
+++ SimulatorLQR.m
@@ -8,7 +8,9 @@ load("matfiles\IP_MODEL.mat")
 % State = [alpha(x), alpha rate, Beta(z), Beta rate, motor current]
 Qr = diag([10,0,1,0,0]); %Weight Matrix for x 
 
-Rr = 1; %Weight for the input variable (Motor voltage)
+Rr = 10; %Weight for the input variable (Motor voltage) 
+% Lower bound = 0.01
+% Upper bound = 10 (it doesnt change anymore)
 
 K = lqr(A, B, Qr, Rr); %Calculate feedback gain 
 
@@ -17,8 +19,11 @@ x0=[0.3 0 0.1 0 0].';
 % D=[0 0 0 0 0].'; % For identity matrix 
 
 % Noise
-use_output_noise = 1;
-use_input_noise = 1;
+use_output_noise = 0;
+output_noise_power = 10;
+
+use_input_noise = 0;
+input_noise_power = 10;
 
 T=2; % Time duration of the simulation 
 out = sim("SimulatorLQRmodel",T);
@@ -36,6 +41,9 @@ legend
 legend("alpha(x)","beta(z)")
 
 figure
-plot(out.t,out.u,"LineWidth",1.5)
+plot(out.t,out.u,out.t,out.u_sat,"LineWidth",1.5)
+% plot(out.t,out.u_sat,"LineWidth",1.5)
 xlabel("Time","Fontsize",14)
 ylabel("Input(Volts)","Fontsize",14)
+legend
+legend("Controller u","Saturated u")
