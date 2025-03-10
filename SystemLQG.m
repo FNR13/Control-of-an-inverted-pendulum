f027fcd5d579ad17c748c8901c927c3076dbe314
FNR13 <152200095+FNR13@users.noreply.github.com>
2025-03-10 14:04:30 +0000

    lab

--- SystemLQG.m
+++ SystemLQG.m
@@ -129,48 +129,6 @@ Re = rv*diag([1,1]); %Variance of measurement errors
 L = lqe(A, G, C, Qe, Re); %Calculate estimator gains
 
 %---------------------------------------------------------------------- 
-%% Simulation parameters 
 x0=[pi/2 0 0.1 0 0].'; 
 D_O= zeros(1,2); % For identity matrix 
-
-% Noise
-use_output_noise = 1;
-output_noise_power = 10;
-
-use_input_noise = 0;
-input_noise_power = 1;
-
-T=5; % Time duration of the simulation 
-out = sim('SimulatorLQGmodel',T);
-
-gg=plot(out.t,out.y); 
-set(gg,'LineWidth',1.5) 
-
-title('Output behavior')
-yline(0,'-')
-
-gg=xlabel('Time (s9'); 
-set(gg,'Fontsize',14); 
-
-gg=ylabel('beta (rad)'); 
-set(gg,'Fontsize',14); 
-
-legend
-legend('alpha(x)','beta(z)')
-
-figure
-plot(out.t,out.u,out.t,out.u_sat,'LineWidth',1.5)
-yline(0,'-')
-title('Input behavior')
-xlabel('Time','Fontsize',14)
-ylabel('Input(Volts)','Fontsize',14)
-legend
-legend('Controller u','Saturated u')
-
-figure
-plot(out.t,out.x,out.t,out.x_estimated,'LineWidth',1.5)
-yline(0,'-')
-title('Estimation comparison')
-xlabel('Time','Fontsize',14)
-ylabel('States comparison','Fontsize',14)
 
