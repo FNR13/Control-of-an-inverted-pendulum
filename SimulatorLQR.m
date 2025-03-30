6e3149c56d64d84c30c6875dfa431b38e3537537
FNR13 <152200095+FNR13@users.noreply.github.com>
2025-03-30 20:34:34 +0100

    View output in degrees

--- SimulatorLQR.m
+++ SimulatorLQR.m
@@ -6,17 +6,17 @@ load("matfiles\IP_MODEL.mat")
 
 %% Controller tuning
 % State = [alpha(x), alpha rate, Beta(z), Beta rate, motor current]
-Qr = diag([10,0,1,0,0]); %Weight Matrix for x
+Qr = diag([10,1,10,1,0]); %Weight Matrix for x
 %Qr = diag([100,1,100,10,0.1]); %Weight Matrix for x
 
-Rr = 0.1; %Weight for the input variable (Motor voltage)
+Rr = 1; %Weight for the input variable (Motor voltage)
 % Lower bound = 0.01
 % Upper bound = 10 (it doesnt change anymore)
 
 K = lqr(A, B, Qr, Rr); %Calculate feedback gain 
 
 %% Simulation parameters 
-x0=[pi/2 0 0.1 0 0].'; 
+x0=[1.3 0 -0.3 0 0].'; 
 % D=[0 0 0 0 0].'; % For identity matrix 
 
 % Noise
@@ -27,8 +27,8 @@ use_input_noise = 0;
 input_noise_power = 10;
 
 % Deadzone
-use_deadzone = 1;
-use_deadzone_compensation = 1;
+use_deadzone = 0;
+use_deadzone_compensation = 0;
 
 if use_deadzone
     deadzone_lower = -0.38;
@@ -44,16 +44,18 @@ T=10; % Time duration of the simulation
 out = sim("SimulatorLQRmodel",T);
 
 %% Plotting
-gg=plot(out.t,out.y); 
+y_degrees = rad2deg(out.y);
+gg=plot(out.t,y_degrees); 
+
 set(gg,"LineWidth",1.5) 
 
 title("Output behavior")
 yline(0,'-')
 
-gg=xlabel("Time (s9"); 
+gg=xlabel("Time (s)"); 
 set(gg,"Fontsize",14); 
 
-gg=ylabel("beta (rad)"); 
+gg=ylabel("Output (degrees)"); 
 set(gg,"Fontsize",14); 
 
 legend
@@ -70,7 +72,7 @@ legend("Controller u","Saturated u")
 
 %% Error 
 t_steady = out.t > 5; 
-error = out.y(t_steady,:);
+error = y_degrees(t_steady,:);
 
 RMSE = sqrt(sum(error.^2)/size(error,1))
 
